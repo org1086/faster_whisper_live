a32506c1ffbfee5971e44fe8fb3ac3cf1a85b67e
khanhdv <khanhdv@sonek.vn>
2023-06-08 15:30:48 +0700

    update loggers, to file, to console

--- backend/logger.py
+++ backend/logger.py
@@ -4,7 +4,7 @@ from logging.handlers import RotatingFileHandler
 MAX_BYTES = 10000000 # Maximum size for a log file
 
 def initialize_logger(name: str, level: int):
-    logger = logging.getLogger(name)
+    logger = logging.getLogger(f'{name}_console_log')
     logger.setLevel(level)
 
     # create console handler and set level to debug
@@ -15,18 +15,10 @@ def initialize_logger(name: str, level: int):
     return logger
 
 def initialize_file_logger(name: str, level: int):
-    logger = logging.getLogger(name)
+    logger = logging.getLogger(f'{name}_file_log')
     logger.setLevel(level)
 
-    # create console handler and set level to debug
-    ch = logging.StreamHandler()
-    ch.setLevel(level)
-    logger.addHandler(ch)
-
-    file_log_format = logging.Formatter('%(message)s')
-
     file_handler = RotatingFileHandler('faster_whisper.log', maxBytes=MAX_BYTES)
-    file_handler.setFormatter(file_log_format)
     file_handler.setLevel(level)
     logger.addHandler(file_handler)
 
@@ -34,8 +26,10 @@ def initialize_file_logger(name: str, level: int):
 
 if __name__ == "__main__":
 
+    logger = initialize_logger(__name__, logging.DEBUG)
     file_logger = initialize_file_logger(__name__, logging.DEBUG)
 
+    logger.info("test console logger only")
     file_logger.info("=======================================")
     file_logger.info("test file logger")
     file_logger.info("=======================================")
